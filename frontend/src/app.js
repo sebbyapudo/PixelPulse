0ba5e45d36c49337b0be54c736b927aa7a31b878
sebbyapudo <sebbyapudo@gmail.com>
2024-01-08 15:22:27 +0300

    Refactored code and set up the routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,22 @@
+import Error404Screen from "./screens/Error404Screen.js";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
+import { parseRequestURL } from "./utils.js";
 
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen,
+};
 const router = () => {
+  const request = parseRequestURL;
+  const parseUrl = 
+  (request.resource ? `/${request.resource}` : `/`); + 
+  (request.id ? '/:id' : "") + 
+  (request.verb ? `/${request.verb}`: '')
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;  
   const main = document.getElementById("main-container")
-  main.innerHTML = HomeScreen.render();
+  main.innerHTML = screen.render();
 }
 
 window.addEventListener("load", router)
+window.addEventListener("hashchange", router)
