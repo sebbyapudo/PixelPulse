fa89ac64358d60a616b47b8eee7d2381b8b70fd7
sebbyapudo <sebbyapudo@gmail.com>
2024-01-08 16:16:39 +0300

    Refactored code | Fixed a bug that prevented efective routing

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,20 @@
 import Error404Screen from "./screens/Error404Screen.js";
 import HomeScreen from "./screens/HomeScreen.js";
 import ProductScreen from "./screens/ProductScreen.js";
-import { parseRequestURL } from "./utils.js";
+import { parseRequestUrl } from "./utils.js";
 
 const routes = {
   '/': HomeScreen,
   '/product/:id': ProductScreen,
 };
 const router = () => {
-  const request = parseRequestURL;
+  const request = parseRequestUrl();
   const parseUrl = 
-  (request.resource ? `/${request.resource}` : `/`); + 
+  (request.resource ? `/${request.resource}` : '/') + 
   (request.id ? '/:id' : "") + 
   (request.verb ? `/${request.verb}`: '')
-  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;  
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+
   const main = document.getElementById("main-container")
   main.innerHTML = screen.render();
 }
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,10 @@
-export const parseRequestURL = () => {
+export const parseRequestUrl = () => {
   const url = document.location.hash.toLocaleLowerCase();
   const request = url.split("/");
+
+  return {
+    resource: request[1],
+    id: request[2],
+    action: request[3],
+  };
 }
